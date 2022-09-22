09c5eb27cd3f25edfcdb60a1998956c22dd07be2
vm-shoeb <shoeb.merchant@purplepatch.in>
2022-09-23 00:53:30 +0530

    specify base in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-table-demo/",
+  base: "/tanstack-table/",
   plugins: [react()],
 });
